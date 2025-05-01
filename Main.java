55977ec25158e534c2f99b1c4b9556a9de3bd0c9
anandkhera <anandk3@nycstudents.net>
2025-04-30 21:49:49 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ public class Main{
     public static void main(String[] args) {
         boolean[][] schedule = new boolean[8][60];
         Appointmentbook a = new Appointmentbook(schedule);
-     //   for(int i = 10; i < 15; i++) schedule[1][i] = true;
-     //   a.printPeriod(2);
-     //   a.findFreeBlock(2, 15);
+        for(int i = 10; i < 15; i++) schedule[1][i] = true;
+        a.printPeriod(2);
+        a.findFreeBlock(2, 15);
         for(int i =25; i <30; i++) schedule[1][i] = true;
         for(int i =0; i <15; i++) schedule[2][i] = true;
         for(int i =41; i <60; i++) schedule[2][i] = true;
